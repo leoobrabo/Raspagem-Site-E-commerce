f055f63a59e11b0395f0af7b75296c201e02d4f4
leoobrabo <leonardomotta2010@hotmail.com>
2021-04-05 16:54:51 -0300

    configurado para envio de emails

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Raspagem-Site-E-commerce
+
 Varrer todos os nomes de cada modelo e o preço atual de cada um deles e na sequência, inserir estes dados dentro de uma planilha no excel.
+
+1 - Seu programa deve iniciar permitindo que o(a) usuário(a) digite o e-mail no qual deseja receber o relatório de valores dos celulares antes do programa iniciar.
+
+2 - Ao iniciar seu programa deverá navegar até o site mencionado(https://telefonesimportados.netlify.app) e extrair os nomes e valores de cada celular cadastrados no site e os ir inserindo em uma planilha(esta planilha é o relatório que estaremosenviando para o nosso cliente no passo a seguir).
+
+3 - O seu programa deve parar de buscar quando chegar a página final
+
+4 - Envie este relatório de preços em anexo para o e-mail que foi inserido no inicio do programa.
--- scrappy_site.py
+++ scrappy_site.py
@@ -4,78 +4,102 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import _find_elements
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import *
-from selenium.webdriver.support import expected_conditions as condicao_esperada
 from time import sleep
 import openpyxl
+import smtplib
+from email.message import EmailMessage
 
 
-def raspagem_de_dados():
+class Scrappy:
 
-    email = input(
-        'Digite o email para receber o relatorio de valores dos celulares!')
-    email.lower()
+    def iniciar(self):
+        self.email_usuario()
+        self.raspagem_de_dados()
+        self.criar_planilha()
+        self.enviar_email_cliente()
 
-    lista_nome_celulares = []
-    lista_preco_celulares = []
+    def email_usuario(self):
+        self.email = input(
+            'Digite o email para receber o relatorio de valores dos celulares!\n')
+        self.email.lower()
 
-    chrome_options = Options()
-    chrome_options.add_argument('--lang=pr-BR')
-    chrome_options.add_argument('--disable-notifications')
-    path = r'G:\Dropbox\OHomemnãoparaNunca\curso_automacao\web_scraping_olx_excel\chromedriver.exe'
-    driver = webdriver.Chrome(executable_path=path, options=chrome_options)
-    link = 'https://telefonesimportados.netlify.app/'
-    driver.get(link)
-    sleep(5)
+    def raspagem_de_dados(self):
+        chrome_options = Options()
+        chrome_options.add_experimental_option(
+            'excludeSwitches', ['enable-logging'])
+        chrome_options.add_argument('--lang=pr-BR')
+        chrome_options.add_argument('--disable-notifications')
+        self.executable_path = r'G:\Dropbox\OHomemnãoparaNunca\curso_automacao\web_scraping_olx_excel\chromedriver.exe'
+        self.driver = webdriver.Chrome(
+            executable_path=self.executable_path, options=chrome_options)
+        self.link = 'https://telefonesimportados.netlify.app/'
+        self.lista_nome_celulares = []
+        self.lista_preco_celulares = []
+        self.driver.get(self.link)
+        sleep(2)
+        for p in range(5):
+            item = 1
+            for i in range(12):
+                lista_nomes = self.driver.find_elements_by_xpath(
+                    f'/html/body/div[5]/div[2]/div[1]/div[{item}]/div/h2/a')
+                self.lista_nome_celulares.append(lista_nomes[0].text)
+                sleep(1)
+                lista_precos = self.driver.find_elements_by_xpath(
+                    f'//div[{item}]/div[@class="single-shop-product" and 1]/div[@class="product-carousel-price" and 2]/ins[1]')
+                self.lista_preco_celulares.append(lista_precos[0].text)
+                item += 1
+                sleep(1)
+            try:
+                botao_proximo = self.driver.find_element_by_xpath(
+                    '/html/body/div[5]/div[2]/div[2]/div/div/nav/ul/li[7]/a')
+                botao_proximo.click()
+                print(f'\u001b[32m{"Navegando para proxima pagina"}\u001b[0m')
+                sleep(2)
 
-    for p in range(5):
+            except NoSuchElementException:
 
-        item = 1
+                print(f'\u001b[33m{"Não há mais paginas!"}\u001b[0m')
+                print(f'\u001b[32m{"Escaneamento Concluido"}\u001b[0m')
 
-        for i in range(12):
+    def criar_planilha(self):
+        index = 2
+        planilha = openpyxl.Workbook()
+        celulares = planilha['Sheet']
+        celulares.title = 'Celulares'
+        celulares['A1'] = 'Nome'
+        celulares['B1'] = 'Preço'
+        for nome, preco in zip(self.lista_nome_celulares, self.lista_preco_celulares):
+            celulares.cell(column=1, row=index, value=nome)
+            celulares.cell(column=2, row=index, value=preco)
+            index += 1
+        planilha.save("planilha_de_preços.xlsx")
 
-            lista_nomes = driver.find_elements_by_xpath(
-                f'/html/body/div[5]/div[2]/div[1]/div[{item}]/div/h2/a')
+        print(f'\u001b[32m{"Planilha criada com sucesso"}\u001b[0m')
 
-            lista_nome_celulares.append(lista_nomes[0].text)
+    def enviar_email_cliente(self):
+        endereco = 'Digite o email aqui'
+        senha = 'Digite a senha aqui'
+        msg = EmailMessage()
+        msg['Subject'] = 'planilha de Preços de Telefones Importados'
+        msg['From'] = endereco
+        msg['To'] = self.email
+        msg.set_content('Olá a sua planilha chegou')
+        arquivos = ["planilha_de_preços.xlsx"]
+        for arquivo in arquivos:
+            with open(arquivo, 'rb') as arq:
+                dados = arq.read()
+                nome_arquivo = arq.name
+            msg.add_attachment(dados, maintype='application',
+                               subtype='octet-stream', filename=nome_arquivo)
+        server = smtplib.SMTP('imap.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(endereco, senha, initial_response_ok=True)
+        server.send_message(msg)
+        print(f'\u001b[32m{"Enviando email para para destinatario"}\u001b[0m')
 
-            sleep(2)
-
-            lista_precos = driver.find_elements_by_xpath(
-                f'//div[{item}]/div[@class="single-shop-product" and 1]/div[@class="product-carousel-price" and 2]/ins[1]')
-
-            lista_preco_celulares.append(lista_precos[0].text)
-
-            item += 1
-
-            sleep(2)
-
-        try:
-
-            botao_proximo = driver.find_element_by_xpath(
-                '/html/body/div[5]/div[2]/div[2]/div/div/nav/ul/li[7]/a')
-            botao_proximo.click()
-            print('Navegando para proxima pagina')
-            sleep(5)
-
-        except NoSuchElementException:
-
-            print('Não há mais paginas')
-            print('Escaneamento Concluido')
-
-    index = 2
-    planilha = openpyxl.Workbook()
-    celulares = planilha['Sheet']
-    celulares.title = 'Celulares'
-    celulares['A1'] = 'Nome'
-    celulares['B1'] = 'Preço'
-
-    for nome, preco in zip(lista_nome_celulares, lista_preco_celulares):
-
-        celulares.cell(column=1, row=index, value=nome)
-        celulares.cell(column=2, row=index, value=preco)
-        index += 1
-
-    planilha.save("exemplos2.xlsx")
+        server.quit()
 
 
-raspagem_de_dados()
+start = Scrappy()
+start.iniciar()
